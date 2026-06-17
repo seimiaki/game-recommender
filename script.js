356e8cb1a8ddd7d776735fa17d7e4a83b1a66c1d
seimiaki <seimiakii@gmail.com>
2026-06-17 14:55:51 +0800

    NEXTPLAY v0.4 - Interactive Game Result Modal System

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const button = document.getElementById("recommend-btn");
 const clickSound = new Audio("assets/sounds/click.mp3");
+const modal = document.getElementById("modal");
+const closeButton = document.getElementById("close-btn");
 
 button.addEventListener("click", function(){
     clickSound.currentTime = 0; 
     clickSound.play();
+});
+
+button.addEventListener("click", function() {
+    modal.classList.remove("hidden");
+});
+
+
+closeButton.addEventListener("click", function() {
+    modal.classList.add("hidden");
 });
