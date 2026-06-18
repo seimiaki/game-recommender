610b7fe9504eb3b8f81578b6adfa2b5bea8f8904
seimiaki <seimiakii@gmail.com>
2026-06-18 23:06:01 +0800

    NEXTPLAY v0.5- Added Multi-Category Selection System

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const button = document.getElementById("recommend-btn");
 const clickSound = new Audio("assets/sounds/click.mp3");
 const modal = document.getElementById("modal");
 const closeButton = document.getElementById("close-btn");
+const categoryButtons = document.querySelectorAll(".category-btn");
 
 button.addEventListener("click", function(){
     clickSound.currentTime = 0; 
     clickSound.play();
 });
-
 button.addEventListener("click", function() {
     modal.classList.remove("hidden");
 });
 
-
 closeButton.addEventListener("click", function() {
     modal.classList.add("hidden");
+});
+
+categoryButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        button.classList.toggle("selected");
+    });
 });
